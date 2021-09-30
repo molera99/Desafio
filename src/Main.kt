3d32fbc6210da030e66136d84fe2f8c6000c83c3
David <molera.ps4@gmail.com>
2021-09-30 13:46:46 +0200

    Rama otroDesarrollo

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,5 @@ fun main(){
     println("prueba")
     var p:persona=persona("DAM2")
     var a:animal=animal("chucho")
+    var n:Otra=Otra("nada")
 }
---
+++ src/Otra.kt
@@ -0,0 +1,1 @@
+data class Otra(var nada:String)
